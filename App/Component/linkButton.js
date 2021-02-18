8181080c4f9f3d644810612379452ebecf392495
m0bad <mobad.cai@gmail.com>
2021-02-18 16:56:45 +0200

    refactored link button component.

--- App/Component/linkButton.js
+++ App/Component/linkButton.js
@@ -1,51 +1,60 @@
-import React from 'react'
-import { View, Text, Platform, TextInput, StyleSheet, Animated, TouchableOpacity, } from 'react-native';
-import { useAnimation } from '../assets/Animation/animation'
-import {Fonts, Metrics, Colors} from '../Themes'
+import React from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { Colors, Fonts } from '../Themes';
 import { ActivityIndicator } from 'react-native-paper';
+import {
+  perfectFont,
+  perfectHeight,
+  perfectWidth,
+} from '../utils/commonFunctions';
 
-const LinkButton = (props) => {
-    const animtion = useAnimation({ doAnimation: props.doAnimation, duration:550})
-    return (
-    <Animated.View style={[styles.container, props.containerStyle]}>
-        <View style={styles.row}>
-            <TouchableOpacity disabled={props.loading} onPress={props.onPress} >
-                { props.loading ? 
-                    <ActivityIndicator style={{paddingHorizontal: 8}} color={Colors.primaryGreen} />:
-                    <Text style={[Fonts.style.normal, styles.inputStyle, props.textStyle]} >{props.buttonText}</Text>
-                }
-            </TouchableOpacity>
-            
-        </View>
-    </Animated.View>
-)}
+const LinkButton = props => (
+  <View style={[styles.container, props.containerStyle]}>
+    <View style={styles.row}>
+      <TouchableOpacity disabled={props.loading} onPress={props.onPress}>
+        {props.loading ? (
+          <ActivityIndicator
+            style={{ paddingHorizontal: 8 }}
+            color={Colors.primaryGreen}
+          />
+        ) : (
+          <Text
+            style={[Fonts.style.normal, styles.inputStyle, props.textStyle]}>
+            {props.buttonText}
+          </Text>
+        )}
+      </TouchableOpacity>
+    </View>
+  </View>
+);
 
 const styles = StyleSheet.create({
-    container:{
-        // width: Metrics.screenWidth * 0.84,
-        alignSelf:'center',
-        justifyContent:'center',
-        height: 50,
-        borderRadius: 8,
-        borderStyle: "solid",
-        // borderWidth: 1,
-        // borderColor: Colors.cloudyBlue
+  container: {
+    alignSelf: 'center',
+    justifyContent: 'center',
+    height: perfectHeight(50),
+    borderRadius: perfectWidth(8),
+    borderStyle: 'solid',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  inputStyle: {
+    width: '100%',
+    fontSize: perfectFont(12),
+    fontWeight: Platform.OS === 'android' ? '400' : 'bold',
+    fontStyle: 'normal',
+    lineHeight: 16,
+    letterSpacing: 0,
+    textAlign: 'right',
+    color: Colors.black,
+  },
+});
 
-    },
-    row:{
-        flexDirection:'row',
-    },
-    inputStyle: {
-        width: '100%',
-        fontSize: 12,
-        fontWeight: Platform.OS === 'android'?'400': "bold",
-        fontStyle: "normal",
-        lineHeight: 16,
-        letterSpacing: 0,
-        textAlign: "right",
-        color: Colors.black,
-        // paddingEnd:17
-    }
-})
-
-export default (LinkButton)
+export default LinkButton;
