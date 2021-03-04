91cad6eb6e1ed84a17d13c9969c7577d3cb49061
m0bad <mobad.cai@gmail.com>
2021-03-04 11:44:50 +0200

    add empty text in real estate details.

--- App/Component/RealestateTypeList.js
+++ App/Component/RealestateTypeList.js
@@ -1,82 +1,97 @@
-import React, {useState} from 'react'
-import { View, Text, TextInput, StyleSheet, Animated, TouchableOpacity, Image } from 'react-native';
-import { useAnimation } from '../assets/Animation/animation'
-import {Fonts, Metrics, Colors, Images} from '../Themes'
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Animated,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import { useAnimation } from '../assets/Animation/animation';
+import { Fonts, Metrics, Colors, Images } from '../Themes';
 import * as Progress from 'react-native-progress';
-import RealestateType from './realestateType'
-const Button = (props) => {
-    const animtion = useAnimation({ doAnimation: props.doAnimation, duration:550})
-    const [selectedRealestateType, setSelectedRealestateType]= useState(props.selectedTypeByProps && props.selectedTypeByProps || '')
+import RealestateType from './realestateType';
+const Button = props => {
+  const [selectedRealestateType, setSelectedRealestateType] = useState(
+    (props.selectedTypeByProps && props.selectedTypeByProps) || '',
+  );
 
+  const [realestateType, setRealestatTypes] = useState([
+    // { _id: 1, nameAr: 'الكل'},
+    { _id: 2, nameAr: 'فيلا' },
+    { _id: 5, nameAr: 'تجاري' },
+    { _id: 6, nameAr: 'كراج' },
+    { _id: 4, nameAr: 'قطعة أرض' },
+    { _id: 3, nameAr: 'شقق سكنية' },
+  ]);
 
-    const [ realestateType, setRealestatTypes ] = useState(
-        [
-            // { _id: 1, nameAr: 'الكل'},
-            { _id: 2, nameAr: 'فيلا'},
-            { _id: 5, nameAr: 'تجاري'},
-            { _id: 6, nameAr: 'كراج'},
-            { _id: 4, nameAr: 'قطعة أرض'},
-            { _id: 3, nameAr: 'شقق سكنية'},
-        ]
-    )
-
-    // const [selectedFeatures, setSelectedFeatures]= useState( props.selectedTypeByProps && selectedTypeByProps || [])
-
-    const [ features, setFeatures ] = useState(
-        [
-            { _id: 1, nameAr: 'انترنت'},
-            { _id: 2, nameAr: 'صالة جيم'},
-            { _id: 5, nameAr: 'بالقرب من مدرسة '},
-            { _id: 4, nameAr: 'كراج', },
-            { _id: 3, nameAr: 'حوض سباحة'},
-            { _id: 6, nameAr: 'نوافذ مبطنة'}
-        ]
-    )
-
-    const handleMarkerPress = (item) => {
-        const index = features.findIndex(ite => ite._id === item._id)
-        features[index]={...features[index], selected: true}
-        let arr = []
-        features.forEach(item0 => item0._id === item._id ? arr.push({...features[index], selected: !item.selected}):arr.push(item0))
-        setFeatures(arr)
-    }
-    
-    console.log('Hello props', props)
-
-    return (
-    <Animated.View style={[styles.container, props.containerStyle,]}>
-        {
-            props.features?
-            <View style={{flexDirection: 'row', flexWrap: 'wrap',  justifyContent: 'flex-end', width: '100%', marginHorizontal: 20}} >
-                {
-                    (props.features && props.features).map( (item, index) => {
-                        const selected1 = props.forEditeng ?  (props.selectedFeatures || []).findIndex(i => i === item ): (props.selectedFeatures || []).findIndex(i => i._id === item._id)
-                        return <RealestateType key={index} forEditeng={props.forEditeng} doAnimation={true} item={item} index={index} onPress={()=> props.onItemPress(item)} selected={ selected1 !== -1 } />} )
-                }
-            </View>
-            :
-            <View style={{flexDirection: 'row-reverse', flexWrap: 'wrap',  justifyContent: 'flex-start', width: '100%'}} >
-                {
-                    ( props.types || realestateType).map( (item, index) => <RealestateType key={index} doAnimation={true} item={item} index={index} onPress={()=> { setSelectedRealestateType(item); props.selectedType(item); }} selected={ selectedRealestateType._id === item._id } /> )
-                }
-            </View>
-        }
+  return (
+    <Animated.View style={[styles.container, props.containerStyle]}>
+      {props.features ? (
+        <View
+          style={{
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            justifyContent: 'flex-end',
+            width: '100%',
+            marginHorizontal: 20,
+          }}>
+          {(props.features && props.features).map((item, index) => {
+            const selected1 = props.forEditeng
+              ? (props.selectedFeatures || []).findIndex(i => i === item)
+              : (props.selectedFeatures || []).findIndex(
+                  i => i._id === item._id,
+                );
+            return (
+              <RealestateType
+                key={index}
+                forEditeng={props.forEditeng}
+                doAnimation={true}
+                item={item}
+                index={index}
+                onPress={() => props.onItemPress(item)}
+                selected={selected1 !== -1}
+              />
+            );
+          })}
+        </View>
+      ) : (
+        <View
+          style={{
+            flexDirection: 'row-reverse',
+            flexWrap: 'wrap',
+            justifyContent: 'flex-start',
+            width: '100%',
+          }}>
+          {(props.types || realestateType).map((item, index) => (
+            <RealestateType
+              key={index}
+              doAnimation={true}
+              item={item}
+              index={index}
+              onPress={() => {
+                setSelectedRealestateType(item);
+                props.selectedType(item);
+              }}
+              selected={selectedRealestateType._id === item._id}
+            />
+          ))}
+        </View>
+      )}
     </Animated.View>
-)}
+  );
+};
 
 const styles = StyleSheet.create({
-    container:{
-        alignItems:'flex-end',
-        justifyContent:'center',
-        alignSelf:'center',
-        flexDirection: 'row',
-    },
+  container: {
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    flexDirection: 'row',
+  },
 
-    textStyle: {
-    
-    }
-   
-})
+  textStyle: {},
+});
 
-export default (Button)
-
+export default Button;
--- App/screens/MainScreens/Offices/OfficeDetails.js
+++ App/screens/MainScreens/Offices/OfficeDetails.js
@@ -1,135 +1,143 @@
-import React from 'react';
-import { View,StyleSheet,Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
 import { ThemeContext } from 'react-navigation';
 import { connect } from 'react-redux';
 import CardItem from '../../../Component/officeCard';
 import FilterButton from '../../../Component/core/buttons/FilterButton';
 import RealEstatList from '../../../Component/realEstateList';
-import api  from '../../../Services/API'
+import api from '../../../Services/API';
 import Header from '../../../Component/Header';
-import {Fonts} from '../../../Themes'
+import { Fonts } from '../../../Themes';
+import VerticalSpace from '../../../Component/core/layouts/VerticalSpace';
+import { perfectFont } from '../../../utils/commonFunctions';
 
-
-class OfficeDetails extends React.Component {
-  static contextType = ThemeContext;
-
-  state ={
-    realEstateListData: [],
-    statuses: []
+const getPropertyUrl = (office, status) => {
+  let url = `${api.apiURL}/get-realestate-agent-details?`;
+  if (office.user_id) {
+    url += `userId=${office.user_id}`;
+  } else {
+    url += `placeId=${office.place_id}`;
+  }
+  if (status) {
+    url += `&type=${status}`;
   }
 
-  async componentDidMount(){
-      try {
-          //get realestate statuses
-          let statusesReq = await fetch(`${api.apiURL}/realEstate/getRealEstateStatus`)
-          let statuses = await statusesReq.json();
-          this.setState({statuses});
+  return url;
+};
+const OfficeDetails = props => {
+  const office = props.navigation.getParam('officeDetails');
+  const [availableStatuses, setAvailableStatuses] = useState([]);
+  const [realEstateProperties, setRealEstateProperties] = useState([]);
+  const [selectedStatus, setSelectedStatus] = useState(null);
 
-          let url = `${api.apiURL}/get-realestate-agent-details?`;
-          let office = this.props.navigation.getParam('officeDetails');
-          if(office.user_id){
-            url+=`userId=${office.user_id}`
-          }else{
-            url+=`placeId=${office.place_id}`
-          }
-          if(this.state.status){
-            url+=`&type=${this.state.status}`
-          }
-          let properties = await fetch(url);
-          let result =  await properties.json()
-          this.setState({realEstateListData: result.properties})
-          
-      } catch (error) {
-        console.log(error.message);  
-          
-      }
+  useEffect(() => {
+    fetch(`${api.apiURL}/realEstate/getRealEstateStatus`).then(result =>
+      result.json().then(data => setAvailableStatuses(data)),
+    );
 
-  }
-  buttonPressed = ()=>{
+    fetch(getPropertyUrl(office, selectedStatus)).then(result =>
+      result.json().then(data => {
+        setRealEstateProperties(data.properties || data.result.properties);
+      }),
+    );
+  }, [office, selectedStatus]);
 
-  }
-  backPressed = ()=>{
-    this.props.navigation.goBack()
-  }
-
-  render() {
-    const item = this.props.navigation.getParam('officeDetails')
-    return (
-        <View style={[styles.mainContainer]}>
-            <Header
-                 noBackButton={false}
-                 headerTitle={item.name}
-                 withSearch={false}
-                 openSearch={false}
-                 onBackPress={this.backPressed}
+  return (
+    <View style={[styles.mainContainer]}>
+      <Header
+        noBackButton={false}
+        headerTitle={office.name}
+        withSearch={false}
+        openSearch={false}
+        onBackPress={() => props.navigation.goBack()}
+      />
+      <View style={[styles.bodyContainer]}>
+        <CardItem
+          onItemPress={() => ({})}
+          onFavPress={() => ({})}
+          doAnimation={true}
+          showCounts={true}
+          showContactButtons={true}
+          selectedOffice={office}
+        />
+      </View>
+      <Text style={[styles.propertiesTitle]}>
+        العقارات المتاحة ل{office.name}
+      </Text>
+      <View style={[styles.buttonGroup]}>
+        <FilterButton
+          onPress={() => setSelectedStatus(null)}
+          selected={selectedStatus === null}
+          label={'الكل'}
+        />
+        {availableStatuses?.map(status => {
+          return (
+            <FilterButton
+              onPress={() => setSelectedStatus(status._id)}
+              label={status.nameAr}
+              selected={status._id === selectedStatus}
             />
-            <View style={[styles.bodyConteiner]} >
-            <CardItem
-            onItemPress={selectedItem => props.onItemPress(selectedItem)}
-            onFavPress={() => props.onFavPress(item)}
-            doAnimation={true}
-            showCounts={true}
-            showContactButtons={true}
-            selectedOffice={item}
-            />
-            <Text style={[styles.propertiesTitle]}>العقارات المتاحة ل{item.name}</Text>
-            <View style={[styles.buttonGroup]}>
-                <FilterButton  onPress={()=>{}} selected={true} label={"الكل"}></FilterButton>
-                {this.state.statuses.map((status,index)=>{
-                    return <FilterButton onPress={this.buttonPressed} label={status.nameAr}/>
-                })}
-            </View>
-            <RealEstatList
-                handleGetMoreDatat={this.handleGetMoreDatat}
-                numberOfRealEstate={this.state.numberOfRealEstate}
-                onItemPress={v => this.handleCardPress(v)}
-                realestateData={this.state.realEstateListData}
-                onMapButtonPress={this.handleViewPress}
-            />
-
-            </View>
-
-        </View>
-    )
-  }
-}
-
+          );
+        })}
+      </View>
+      {realEstateProperties?.length ? (
+        <RealEstatList
+          handleGetMoreDatat={() => ({})}
+          numberOfRealEstate={realEstateProperties?.length || 0}
+          onItemPress={() => ({})}
+          realestateData={realEstateProperties}
+          onMapButtonPress={() => ({})}
+        />
+      ) : (
+        <>
+          <VerticalSpace height={100} />
+          <Text style={styles.empty}>لا توجد عقارات</Text>
+        </>
+      )}
+    </View>
+  );
+};
 
 export default OfficeDetails;
 
 const styles = StyleSheet.create({
-    mainContainer:{
-        flexDirection: 'column',
-        alignItems: 'center',
+  mainContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  bodyContainer: {
+    paddingTop: 10,
+    paddingHorizontal: 5,
+  },
+  buttonGroup: {
+    width: '90%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 10,
+    backgroundColor: '#ffffff',
+    borderRadius: 6,
+    flexDirection: 'row-reverse',
+    shadowColor: 'rgba(0, 0, 0, 0.08)',
+    shadowOffset: {
+      width: 0,
+      height: 7,
     },
-    bodyConteiner:{
-        paddingTop: 10,
-        paddingHorizontal: 5
-    },
-    buttonGroup:{
-        width: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingVertical: 10,
-        backgroundColor: '#ffffff',
-        borderRadius: 6,
-        flexDirection: 'row-reverse',
-        shadowColor: 'rgba(0, 0, 0, 0.08)',
-        shadowOffset: {
-          width: 0,
-          height: 7,
-        },
-        marginTop: 5,
-        shadowRadius: 15,
-        shadowOpacity: 1,
-        elevation: 2
-    },
-    propertiesTitle:{
-        fontFamily: Fonts.style.normal.fontFamily,
-        fontSize: 11,
-        textAlign: 'right',
-        alignSelf: 'flex-end',
-        padding: 12,
-        paddingVertical: 10
-    }
-})
+    marginTop: 5,
+    shadowRadius: 15,
+    shadowOpacity: 1,
+    elevation: 2,
+  },
+  propertiesTitle: {
+    fontFamily: Fonts.style.normal.fontFamily,
+    fontSize: 11,
+    textAlign: 'right',
+    alignSelf: 'flex-end',
+    padding: 12,
+    paddingVertical: 10,
+  },
+  empty: {
+    fontSize: perfectFont(22),
+    textAlign: 'center',
+    alignSelf: 'center',
+  },
+});
